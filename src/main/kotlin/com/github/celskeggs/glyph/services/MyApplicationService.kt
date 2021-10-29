59ddb62da95fc9eefa11feb5068df4011f518de5
GitHub Action <action@github.com>
2021-10-29 23:27:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/celskeggs/glyph/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/celskeggs/glyph/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.celskeggs.glyph.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.celskeggs.glyph.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/celskeggs/glyph/services/MyProjectService.kt
+++ src/main/kotlin/com/github/celskeggs/glyph/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.celskeggs.glyph.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.celskeggs.glyph.MyBundle
 
 class MyProjectService(project: Project) {
 
